c3251ed2c187c4da09ff3b0d9bf215313a569c6c
ajwilsonvt <ajwilsonvt@users.noreply.github.com>
2017-02-09 15:01:42 -0500

    moved logic from view to controller

--- src/app/controllers/pages_controller.rb
+++ src/app/controllers/pages_controller.rb
@@ -4,8 +4,16 @@ class PagesController < ApplicationController
 
   def home
       @o_string = params["string"]
-      # @o_string.string_append! would modify @o_string
-      # @o_string.string_append would NOT modify @o_string
+
+      # blank? is a rails method not in regular ruby
+      # unless x == if !(x)
+      @a_string
+      unless @o_string.blank?
+        @a_string = @o_string.string_append
+
+        # @o_string.string_append! would modify @o_string
+        # @o_string.string_append would NOT modify @o_string
+      end
   end
 
   def contact
